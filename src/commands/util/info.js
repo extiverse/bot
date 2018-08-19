d4b5ce3d3f2b1d06acc8032d241ac7c29fc1c8a1
David Sevilla <dsevilla192@icloud.com>
2018-08-19 11:41:07 -0400

    Add info command

---
+++ src/commands/util/info.js
@@ -0,0 +1,42 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+const { stripIndents } = require('common-tags');
+const moment = require('moment');
+require('moment-duration-format');
+
+class StatsCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'info',
+      group: 'util',
+      memberName: 'info',
+			aliases: ['stats'],
+      description:
+        'Displays statistics about the bot.',
+      clientPermissions: ['EMBED_LINKS'],
+      ratelimit: 2,
+			guarded: true,
+		});
+
+		this.uptime = new Date();
+	}
+
+	run(msg) {
+		const embed = new RichEmbed()
+			.setColor(3447003)
+			.setTitle('Flagrow Bot Statistics')
+			.addField('❯ Uptime', moment.duration(this.client.uptime).format('d[d ]h[h ]m[m ]s[s]'), true)
+			.addField('❯ Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`, true)
+			.addField('❯ Contributors', stripIndents`
+				• [@luceos](https://github.com/luceos)
+				• [@datitisev](https://github.com/datitisev)
+			`, true)
+			.addField('❯ Source Code', '[@flagrow/bot](https://github.com/flagrow/bot)', true)
+			.setThumbnail(this.client.user.displayAvatarURL)
+			.setFooter('© 2018 Flagrow');
+
+		return msg.embed(embed);
+	}
+}
+
+module.exports = StatsCommand;
