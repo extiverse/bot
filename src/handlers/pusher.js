1721b86a967b3c7409f9d34134d3b4711b342542
David Sevilla <dsevilla192@icloud.com>
2018-08-21 15:12:15 -0400

    lol

--- src/handlers/pusher.js
+++ src/handlers/pusher.js
@@ -38,38 +38,4 @@ module.exports = client => {
 
     send(embed);
   });
-
-  setTimeout(
-    () =>
-      pusher.emit('newPackageReleased', {
-        package: {
-          packagist_hash: 1398824876,
-          name: 'reflar/level-ranks',
-          description: 'Add a level/expierence bar to your flarum Forum.',
-          license: null,
-          type: 'flarum-extension',
-          stars: 0,
-          forks: 0,
-          downloads: 90,
-          dependents: 0,
-          suggesters: 0,
-          title: 'ReFlar Level Ranks',
-          icon: { name: 'percent', backgroundColor: '#f4f4f4', color: '#000' },
-          highest_version: '1.0',
-          abandoned_for: null,
-          vcs: 'https://github.com/ReFlar/level-ranks',
-          created_at: '2018-08-11 21:39:58',
-          updated_at: '2018-08-21 13:19:03',
-          id: 436,
-          bazaarId: 'reflar$level-ranks',
-          lastVersion: null,
-          premiumEnabled: false,
-          discussLink: null,
-          landingPageLink: 'http://flagrow.test/extensions/reflar/level-ranks',
-          locale: null,
-          versions: [],
-        },
-      }),
-    1000
-  );
 };
