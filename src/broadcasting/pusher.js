cc13ec56347dc800a06ec05b7d9ab3d304d8b743
David Sevilla <dsevilla192@icloud.com>
2018-08-21 16:16:05 -0400

    remove console log am smrt ok

--- src/broadcasting/pusher.js
+++ src/broadcasting/pusher.js
@@ -27,8 +27,6 @@ module.exports = class Pusher extends EventEmitter {
           `The event ${e} was received from pusher, seeking handler ${handler}`
         );
 
-        console.log(e, data);
-
         this.emit(handler, data);
       });
     }
