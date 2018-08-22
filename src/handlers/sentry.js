b85bd35a883872a130cb2c4f97c069fb28e3df60
David Sevilla <dsevilla192@icloud.com>
2018-08-22 10:03:12 -0400

    Don't crash if Sentry is not set up

--- src/handlers/sentry.js
+++ src/handlers/sentry.js
@@ -3,5 +3,7 @@ const Raven = SENTRY_URL && require('raven');
 
 if (Raven) Raven.config(SENTRY_URL).install();
 
-module.exports = callback => Raven.context(callback.bind(this, Raven));
+module.exports = Raven
+  ? callback => Raven.context(callback.bind(this, Raven))
+  : callback();
 module.exports.Raven = Raven;
