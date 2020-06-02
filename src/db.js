d3be1a3818f7884c4e525e60ec337d4f45b15966
David Sevilla Martin <me@datitisev.me>
2020-06-02 16:17:07 -0400

    Remove (un)subscribe commands & db for extension subscriptions

--- src/db.js
+++ src/db.js
@@ -2,19 +2,11 @@ const Enmap = require('enmap');
 const path = require('path');
 const log = require('consola').withScope('db');
 
-const extensionNotifications = new Enmap({
-  name: 'extensionNotifications',
-  dataDir: path.resolve(__dirname, '../data'),
-});
 const discussNotifications = new Enmap({
   name: 'discussNotifications',
   dataDir: path.resolve(__dirname, '../data'),
 });
 
-extensionNotifications.defer.then(() =>
-  log.info('Loaded extension notifications')
-);
-
 discussNotifications.defer.then(() => log.info('Loaded discuss notifications'));
 
-module.exports = { extensionNotifications, discussNotifications };
+module.exports = { discussNotifications };
